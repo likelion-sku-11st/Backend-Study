cad4f963cc7270794ff517bd289b654a108bdd8c
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-08 02:43:18 +0900

    ex3_2

---
+++ Spring/session0403/ex3_2.java
@@ -0,0 +1,10 @@
+public class ex3_2 {    
+  public static void main(String[] args){
+    int sum =0;
+    while(i<=100){
+      sum += i;
+      i++;
+    }
+    System.out.println("1~%d까지의 합 : %d", i-1,sum);
+  }
+}
