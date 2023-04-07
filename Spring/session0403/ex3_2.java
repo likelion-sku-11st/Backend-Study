5078cd2933ba5b2bb44482b8e2bd6d5f94fb1bef
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-08 02:48:58 +0900

    ex3_2

--- Spring/session0403/ex3_2.java
+++ Spring/session0403/ex3_2.java
@@ -1,10 +1,11 @@
-public class ex3_2 {    
-  public static void main(String[] args){
-    int sum =0;
-    while(i<=100){
-      sum += i;
-      i++;
+public class ex3_2 {
+    public static void main(String[] args){
+        int i = 1;
+        int sum = 0;
+        while(i<=100){
+            sum += i;
+            i++;
+        }
+        System.out.printf("1~%d까지의 합 : %d", i-1, sum);
     }
-    System.out.println("1~%d까지의 합 : %d", i-1,sum);
-  }
 }
