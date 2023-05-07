39c1f99e63e3e6e453b0ed0f5f24cae2cdcb0226
Minsun Cho <87747459+MinsunCho-dev@users.noreply.github.com>
2023-05-07 21:51:45 +0900

    Create ex2.java

---
+++ session0501/ex2.java
@@ -0,0 +1,14 @@
+public class ex2 {
+  public static void findEvenOdd(int num) {
+    if (num % 2 == 0)
+      System.out.println(num + "is Even(짝수)");
+    else
+      System.out.println(num "is Odd(홀수)"); 
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("Input : ");
+    int num = sc.nextInt();
+    findEvenOdd(num);
+  }
+}
